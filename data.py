a9e232caceabed4f083362138701c2b24a5e99db
Aleksandr Dmitriev <shurik2533@ya.ru>
2021-12-09 11:52:56 +0300

    disable broken parts

--- data.py
+++ data.py
@@ -4,8 +4,8 @@ import json
 
 from config.db import USER, PASSWORD, HOST, PORT, DATABASE
 
-WINDOW_SHORT = 60
-WINDOW_LONG = 600
+WINDOW_SHORT = 120
+# WINDOW_LONG = 1200
 
 
 def get_log(date_from, date_to):
@@ -20,10 +20,10 @@ def get_log(date_from, date_to):
         log['thermometer_top'] = log['log'].apply(lambda x: x['thermometer']['top'])
         log['thermometer_bottom'] = log['log'].apply(lambda x: x['thermometer']['bottom'])
         log['thermometer_external'] = log['log'].apply(lambda x: x['thermometer']['external'])
-        log['co2'] = log['log'].apply(lambda x: x['co2']['co2'])
+        # log['co2'] = log['log'].apply(lambda x: x['co2']['co2'])
         log['humidity'] = log['log'].apply(lambda x: x['humidity']['humidity'])
-        log['soil_moisture_top'] = log['log'].apply(lambda x: x['soil_moisture']['top'])
-        log['soil_moisture_bottom'] = log['log'].apply(lambda x: x['soil_moisture']['bottom'])
+        # log['soil_moisture_top'] = log['log'].apply(lambda x: x['soil_moisture']['top'])
+        # log['soil_moisture_bottom'] = log['log'].apply(lambda x: x['soil_moisture']['bottom'])
         log['last_watering_time_top'] = pd.to_datetime(log['log'].apply(lambda x: x['last_watering_time']['top']))
         log['last_watering_time_bottom'] = pd.to_datetime(log['log'].apply(lambda x: x['last_watering_time']['bottom']))
         log['light_1'] = log['log'].apply(lambda x: 1 if x['light']['1'] == 'ON' else -1)
@@ -31,13 +31,13 @@ def get_log(date_from, date_to):
         log['fan_top'] = log['log'].apply(lambda x: x['fan']['top'])
         log['fan_bottom'] = log['log'].apply(lambda x: x['fan']['bottom'])
 
-        window_short_columns = ['thermometer_top', 'thermometer_bottom', 'thermometer_external', 'co2', 'humidity',
+        window_short_columns = ['thermometer_top', 'thermometer_bottom', 'thermometer_external', 'humidity',
                                 'fan_top', 'fan_bottom']
-        window_long_columns = ['soil_moisture_top', 'soil_moisture_bottom']
+        # window_long_columns = ['soil_moisture_top', 'soil_moisture_bottom']
         log[window_short_columns] = log[window_short_columns].rolling(window=WINDOW_SHORT).mean()
         log[window_short_columns] = log[window_short_columns].apply(lambda x: round(x, 2))
-        log[window_long_columns] = log[window_long_columns].rolling(window=WINDOW_LONG).mean()
-        log[window_long_columns] = log[window_long_columns].apply(lambda x: round(x, 2))
+        # log[window_long_columns] = log[window_long_columns].rolling(window=WINDOW_LONG).mean()
+        # log[window_long_columns] = log[window_long_columns].apply(lambda x: round(x, 2))
         log = log.dropna()
         del log['log']
         log = log.groupby(pd.Grouper(key='ts', freq='1min')).last().reset_index()
